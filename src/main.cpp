1b5af1f7d413509c7f53310de2ec2641bf8a2790
mj0ln1r42 <baileybp4@gmail.com>
2019-08-07 20:02:10 -0400

    Add serial debugs to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
 #include <H2HDisplay.h>
+#include <H2HTest.h>
 #include <LifeGame.h>
 
 Game* game;
 
-void setup() {
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Begin setup()");
+
     // Choose your Display type
     Display* gameDisplay = new H2HDisplay();
+    Serial.println("gameDisplay created");
 
     // Choose your Game type
-    game = (Game*)new LifeGame(gameDisplay);
+    game = (Game*)new H2HTest(gameDisplay);
+    Serial.println("game created");
+
     game->setup();
+    Serial.println("game setup complete");
+
+    Serial.println("End setup()");
 }
 
 void loop() {
