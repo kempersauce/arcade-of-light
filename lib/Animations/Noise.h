57545f5c2e0c706fb93f22e2b6f3947425464700
mj0ln1r42 <baileybp4@gmail.com>
2019-08-12 23:28:01 -0400

    Break noise generator apart from noise animation

--- lib/Animations/Noise.h
+++ lib/Animations/Noise.h
@@ -1,73 +1,34 @@
+#include <NoiseGenerator.h>
+
 class Noise : Animation
 {
     private:
-      uint16_t x;
-      uint16_t y;
-      uint16_t z;
-
-      // We're using the x/y dimensions to map to the x/y pixels on the matrix.  We'll
-      // use the z-axis for "time".  speed determines how fast time moves forward.  Try
-      // 1 for a very slow moving effect, or 60 for something that ends up looking like
-      // water.
-      // uint16_t speed = 1; // almost looks like a painting, moves very slowly
-      uint16_t speed = 20; // a nice starting speed, mixes well with a scale of 100
-      // uint16_t speed = 33;
-      // uint16_t speed = 100; // wicked fast!
-
-      // Scale determines how far apart the pixels in our noise matrix are.  Try
-      // changing these values around to see how it affects the motion of the display.  The
-      // higher the value of scale, the more "zoomed out" the noise iwll be.  A value
-      // of 1 will be so zoomed in, you'll mostly see solid colors.
-
-      // uint16_t scale = 1; // mostly just solid colors
-      // uint16_t scale = 4011; // very zoomed out and shimmery
-      uint16_t scale = 1024;
-
-      // This is the array that we keep our computed noise values in
-      uint8_t** noise;
-
-      // Fill the x/y array of 8-bit noise values using the inoise8 function.
-      void fillnoise8(Display* display) {
-        for(int i = 0; i < display->numStrips; i++) {
-          int ioffset = scale * i;
-          for(int j = 0; j < display->lengthStrips; j++) {
-            int joffset = scale * j;
-            noise[i][j] = inoise8(x + ioffset,y + joffset,z);
-          }
-        }
-        z += speed;
-      }
+      NoiseGenerator* generator;
 
     public:
       Noise(Display* display) : Animation()
       {
-        noise = new uint8_t*[display->lengthStrips];
-        for (int i = 0; i < display->lengthStrips; i++)
-        {
-          noise[i] = new uint8_t[display->numStrips];
-        }
-
-        // Initialize our coordinates to some random values
-        x = random16();
-        y = random16();
-        z = random16();
+          generator = new NoiseGenerator(display->numStrips, display->numLeds);
       }
 
-      void draw(Display* display){
-        static uint8_t ihue=0;
-        fillnoise8(display);
-        for(int i = 0; i < display->numStrips; i++) {
-          for(int j = 0; j < display->lengthStrips; j++) {
-            // We use the value at the (i,j) coordinate in the noise
-            // array for our brightness, and the flipped value from (j,i)
-            // for our pixel's hue.
-            //leds[i][j] = CHSV(noise[j][i],255,noise[i][j]);
-            display->strips[i][j] = CHSV(ihue + (noise[j][i]>>2),255,noise[i][j]);
-            // You can also explore other ways to constrain the hue used, like below
-            // leds[XY(i,j)] = CHSV(ihue + (noise[j][i]>>2),255,noise[i][j]);
+      void draw(Display* display)
+      {
+          static uint8_t ihue=0;
+          noiseGenerator->fillnoise8();
+          for (int i = 0; i < display->numStrips; i++)
+          {
+              for (int j = 0; j < display->lengthStrips; j++)
+              {
+                  // We use the value at the (i,j) coordinate in the noise
+                  // array for our brightness, and the flipped value from (j,i)
+                  // for our pixel's hue.
+                  //leds[i][j] = CHSV(noise[j][i],255,noise[i][j]);
+                  display->strips[i][j] = CHSV(ihue + (noiseGenerator->noise[j][i]>>2), 255, noiseGenerator->noise[i][j]);
+                  // You can also explore other ways to constrain the hue used, like below
+                  // leds[XY(i,j)] = CHSV(ihue + (noise[j][i]>>2),255,noise[i][j]);
+              }
           }
-        }
-        ihue+=1;
 
+          ihue+=1;
       }
   };
---
+++ lib/Animations/NoiseGenerator.h
@@ -0,0 +1,64 @@
+class NoiseGenerator
+{
+    public:
+      NoiseGenerator(int widt, int heigt)
+      {
+          width = widt;
+          height = heigt;
+
+          noise = new uint8_t*[width];
+          for (int i = 0; i < height; i++)
+          {
+            noise[i] = new uint8_t[display->numStrips];
+          }
+
+          // Initialize our coordinates to some random values
+          x = random16();
+          y = random16();
+          z = random16();
+      }
+
+      int width;
+      int height;
+
+      // uint16_t speed = 1; // almost looks like a painting, moves very slowly
+      uint16_t speed = 20; // a nice starting speed, mixes well with a scale of 100
+      // uint16_t speed = 33;
+      // uint16_t speed = 100; // wicked fast!
+
+      // Scale determines how far apart the pixels in our noise matrix are.  Try
+      // changing these values around to see how it affects the motion of the display.  The
+      // higher the value of scale, the more "zoomed out" the noise iwll be.  A value
+      // of 1 will be so zoomed in, you'll mostly see solid colors.
+
+      // uint16_t scale = 1; // mostly just solid colors
+      // uint16_t scale = 4011; // very zoomed out and shimmery
+      uint16_t scale = 1024;
+
+      // This is the array that we keep our computed noise values in
+      uint8_t** noise;
+
+    private:
+      // We're using the x/y dimensions to map to the x/y pixels on the matrix.  We'll
+      // use the z-axis for "time".  speed determines how fast time moves forward.  Try
+      // 1 for a very slow moving effect, or 60 for something that ends up looking like
+      // water.
+      uint16_t x;
+      uint16_t y;
+      uint16_t z;
+
+      // Fill the x/y array of 8-bit noise values using the inoise8 function.
+      void fillnoise8()
+      {
+          for (int i = 0; i < width; i++)
+          {
+              int ioffset = scale * i;
+              for (int j = 0; j < height; j++)
+              {
+                  int joffset = scale * j;
+                  noise[i][j] = inoise8(x + ioffset, y + joffset, z);
+              }
+          }
+          z += speed;
+      }
+  };
