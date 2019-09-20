9fea2d8e2b2c188d626904d2c6df1a95decafa24
Robby <blumrj@gmail.com>
2019-09-20 19:50:22 -0400

    start stuff

---
+++ lib/Animations/Charge.h
@@ -0,0 +1,62 @@
+#pragma once
+//Dot Class
+//Class that sets a dot in a specific locatio on the LED strip
+class Charge : Animation {
+    public:
+        //location values
+        int xStart;
+        int xLoc;
+        int yStart;
+        int yLoc;
+        //boundaries of movement Area
+        int xMax;
+        int yMax;
+        //colors (HSV)
+        int Hue;
+        int Saturation;
+        int Brightness
+
+        //Constructor - COULD BE MORE EFFICIENT
+        Charge (int startHue, int startX, int startY, int yMaximum)
+        {
+            Hue = startHue;
+            Saturation = 255;
+            Brightness = 200;
+            xStart = startX;
+            xLoc = startX;
+            yStart = startY;
+            yLoc = startY;
+            yMax = yMaximum;
+        }
+
+        //Methods
+        void power(bool powerUp)
+        {
+          if (YStart < yMax)
+            {
+              if (powerUp == true){
+              if(yLoc > yMax){yLoc++;}
+              Saturation = 255 * (yLoc / yMax);
+              } else {
+              if(yLoc > 0){yLoc--;}
+              Saturation = 255 * (yLoc / yMax);
+              }
+            } else {
+              if (powerUp == true){
+              if(yLoc < yMax){yLoc--;}
+              Saturation = 255 * (yLoc / yMax);
+              } else {
+              if(yLoc < 0){yLoc--;}
+              Saturation = 255 * (yLoc / yMax);
+              }
+            }
+        }
+
+        void draw(Display* display)
+        {
+          for (int i; i < yMax; i++){
+            display->strips[xLoc][i] = CHSV(Hue, Saturation, Brightness);
+          }
+
+        }
+};
