5a10122b005eab6d282a5ba28d28831f4b656d72
LukeSteg <lukestegs@gmail.com>
2019-09-27 20:33:07 -0700

    added serial stuff

---
+++ lib/Serial/KemperSerialTranmitter.h
@@ -0,0 +1,30 @@
+
+#include <string>
+#include <queue>
+
+class KemperSerialTransmitter{
+
+  public:
+    HardwareSerial serial;
+    const static byte numChars = 32;  
+    
+    KemperSerialTransmitter(HardwareSerial &serialRef){   
+      serial.begin(9600);
+    }
+    void sendMessage(String msg)
+    {
+      int msgLength = msg.length();
+      {
+        if(msgLength > numChars)
+        {
+          return;
+        }
+        else
+        {
+          String finalMsg = "<"+msg+">";
+          serial.print(finalMsg);
+        }
+      }
+    }
+  
+};
---
+++ lib/Test/SerialTest.h
@@ -0,0 +1,42 @@
+#include <Game.h>
+#include <SingleColorBG.h>
+#include <KemperSerialReceiver.h>
+
+class SerialTest : Game
+{
+    KemperSerialReceiver *ks;
+
+public:
+    SerialTest(Display *gameDisplay)
+        : Game(gameDisplay)
+    {
+        background = (Animation *)new SingleColorBG(0, 0, 255);
+    }
+
+    void setup()
+    {
+        ks = new KemperSerialReceiver(Serial1);
+        delay(4000);
+        pinMode(LED_BUILTIN, OUTPUT);
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(1000);
+        digitalWrite(LED_BUILTIN, LOW);
+        Serial.print("starting Serial Test");
+
+    }
+
+    void loop()
+    {
+        background->draw(display);
+
+        if (ks->recvWithStartEndMarkers())
+        {
+            char output[32];
+            ks->getNextMessage(output);
+
+            Serial.print("\noutput:");
+            Serial.print(output);
+
+        }
+    }
+}
