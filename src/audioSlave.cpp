e3d4f422704a2ae1e97242326e75c0c68cfc8fd7
mj0ln1r42 <baileybp4@gmail.com>
2019-09-30 19:23:56 -0400

    bump up to 5 serials for audio slave

--- src/audioSlave.cpp
+++ src/audioSlave.cpp
@@ -1,72 +1,65 @@
-// #include <Arduino.h>
-// #include <KemperSerialReceiver.h>
+#include <Arduino.h>
+#include <KemperSerialReceiver.h>
 
-// #include <Audio.h>
-// #include <Wire.h>
-// #include <SPI.h>
-// #include <SD.h>
-// #include <SerialFlash.h>
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
 
-// AudioControlSGTL5000 sgtl5000_1;
-// AudioOutputI2S i2s1;
+AudioControlSGTL5000 sgtl5000_1;
+AudioOutputI2S i2s1;
 
-// // Use these with the Teensy Audio Shield
-// //This uses the audio shield's card reader
-// #define SDCARD_CS_PIN    10
-// #define SDCARD_MOSI_PIN  7
-// #define SDCARD_SCK_PIN   14
+// Use these with the Teensy Audio Shield
+//This uses the audio shield's card reader
+#define SDCARD_CS_PIN    10
+#define SDCARD_MOSI_PIN  7
+#define SDCARD_SCK_PIN   14
 
-// AudioPlaySdWav           playSdWav1;
+AudioPlaySdWav           playSdWav1;
+AudioConnection          patchCord1(playSdWav1, 0, i2s1, 0);
+KemperSerialReceiver serialReceiver[5]{
+	KemperSerialReceiver(Serial1),
+	KemperSerialReceiver(Serial2),
+	KemperSerialReceiver(Serial3),
+	KemperSerialReceiver(Serial4),
+	KemperSerialReceiver(Serial5)
+};
 
-// AudioConnection          patchCord1(playSdWav1, 0, i2s1, 0);
+void setup()
+{
+	Serial.begin(115200);
+	AudioMemory(16);
+	sgtl5000_1.enable();
+	sgtl5000_1.volume(1);
+	SPI.setMOSI(SDCARD_MOSI_PIN);
+	SPI.setSCK(SDCARD_SCK_PIN);
+	if (!(SD.begin(SDCARD_CS_PIN)))
+	{
+		while (1)
+		{
+			Serial.println("Unable to access the SD card");
+			delay(500);
+		}
+	}
+	playSdWav1.play("HUMANITY.WAV");
+}
 
-// KemperSerialReceiver serialReceiver[4]{
-// 	KemperSerialReceiver(Serial1),
-// 	KemperSerialReceiver(Serial2),
-// 	KemperSerialReceiver(Serial3),
-// 	KemperSerialReceiver(Serial4)
-// };
-
-// void setup()
-// {
-// 	Serial.begin(115200);
-	
-// 	AudioMemory(16);
-// 	sgtl5000_1.enable();
-// 	sgtl5000_1.volume(1);
-// 	SPI.setMOSI(SDCARD_MOSI_PIN);
-// 	SPI.setSCK(SDCARD_SCK_PIN);
-// 	if (!(SD.begin(SDCARD_CS_PIN)))
-// 	{
-// 		while (1)
-// 		{
-// 			Serial.println("Unable to access the SD card");
-// 			delay(500);
-// 		}
-// 	}
-
-// 	playSdWav1.play("HUMANITY.WAV");
-// }
-
-// void loop()
-// {
-// 	for (int i = 0; i < 4; i++)
-// 	{
-// 		serialReceiver[i].recvWithStartEndMarkers();
-
-// 		while (serialReceiver[i].hasMessages())
-// 		{
-// 			char output[32];
-// 			serialReceiver[i].getNextMessage(output);
-
-// 			Serial.print("playing from serial");
-// 			Serial.print(i + 1);
-// 			Serial.print(": ");
-// 			Serial.println(output);
-
-// 			playSdWav1.play(output);
-
-// 			Serial.println("Done");
-// 		}
-// 	}
-// }
+void loop()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		serialReceiver[i].recvWithStartEndMarkers();
+		while (serialReceiver[i].hasMessages())
+		{
+			char output[32];
+			serialReceiver[i].getNextMessage(output);
+			Serial.print("playing from serial");
+			Serial.print(i + 1);
+			Serial.print(": ");
+			Serial.println(output);
+			playSdWav1.play(output);
+			Serial.println("Done");
+		}
+	}
+}
