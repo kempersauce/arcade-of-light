ed9a90119e98c349a42335fe73b935699d8a5ef8
Jon Bedell <Jonbedell2@gmail.com>
2019-10-04 23:34:28 -0400

    light cleanup

--- src/RocketGame.cpp
+++ src/RocketGame.cpp
@@ -16,18 +16,13 @@ Display* gameDisplay;
 void setup()
 {
     gameDisplay = (Display*)new H2HDisplay();
-
-
     game = (Game*)new RocketGame(gameDisplay);
-    pinMode(9,OUTPUT);
     game->setup();
     FastLED.show();
     Serial5.begin(9600);
-    Serial4.begin(9600);
 }
 void loop()
 {
-    
     game->loop();
     FastLED.show();
 }
