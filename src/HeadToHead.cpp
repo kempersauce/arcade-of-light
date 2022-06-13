ed5b9e27683109c46973040b6e22142559ae4a69
mj0ln1r42 <baileybp4@gmail.com>
2022-06-13 19:56:03 -0400

    Commenting out H2H game for TwentyTest

--- src/HeadToHead.cpp
+++ src/HeadToHead.cpp
@@ -1,25 +1,25 @@
 
-#include <Arduino.h>
-#include <FastLED.h>
-#include <Display.h>
-#include <H2HDisplay.h>
-#include <Game.h>
-#include <Head2Head.h>
+// #include <Arduino.h>
+// #include <FastLED.h>
+// #include <Display.h>
+// #include <H2HDisplay.h>
+// #include <Game.h>
+// #include <Head2Head.h>
 
 
-Game* game;
-Display* gameDisplay;
+// Game* game;
+// Display* gameDisplay;
 
-void setup()
-{
-    gameDisplay = (Display*)new H2HDisplay();
+// void setup()
+// {
+//     gameDisplay = (Display*)new H2HDisplay();
 
-    game = (Game*)new Head2Head(gameDisplay);
-    game->setup();
-    FastLED.show();
-}
-void loop()
-{
-    game->loop();
-    FastLED.show();
-}
+//     game = (Game*)new Head2Head(gameDisplay);
+//     game->setup();
+//     FastLED.show();
+// }
+// void loop()
+// {
+//     game->loop();
+//     FastLED.show();
+// }
