b6070cd1882957398ebef15351d0602fcbc144b4
mj0ln1r42 <baileybp4@gmail.com>
2022-07-12 00:18:07 -0400

    Fix up WavPlayer to use new button configuration

--- lib/Games/WavPlayer.h
+++ lib/Games/WavPlayer.h
@@ -1,5 +1,7 @@
 //working audio example
 
+#include <memory>  // For shared_ptr
+
 #include <Game.h>
 #include <button.h>
 #include <Audio.h>
@@ -32,14 +34,29 @@ class WavPlayer : Game
 private:
 
     //Buttons go here
-    Button ABut = Button(24);
-    Button BBut = Button(25);
-    Button CBut = Button(26);
-    Button DBut = Button(27);
-    Button EBut = Button(28);
-    Button FBut = Button(29);
+    std::shared_ptr<Button> ABut;
+    std::shared_ptr<Button> BBut;
+    std::shared_ptr<Button> CBut;
+    std::shared_ptr<Button> DBut;
+    std::shared_ptr<Button> EBut;
+    std::shared_ptr<Button> FBut;
 
 public:
+    WavPlayer(std::shared_ptr<Button> aBut,
+              std::shared_ptr<Button> bBut,
+              std::shared_ptr<Button> cBut,
+              std::shared_ptr<Button> dBut,
+              std::shared_ptr<Button> eBut,
+              std::shared_ptr<Button> fBut) :
+        ABut{std::move(aBut)},
+        BBut{std::move(bBut)},
+        CBut{std::move(cBut)},
+        DBut{std::move(dBut)},
+        EBut{std::move(eBut)},
+        FBut{std::move(fBut)}
+    {
+    }
+
     void setup()
     {
         Serial.begin(9600);
@@ -50,42 +67,42 @@ public:
     void loop()
     {
        // if (playSdWav1.isPlaying() == false) {
-          if (ABut.IsPressed())
+          if (ABut->IsPressed())
           {
               playSdWav2.play("Guycey1.wav");
               Serial.println("Button A Pressed");
               delay(500);
           }
 
-          if (BBut.IsPressed())
+          if (BBut->IsPressed())
           {
               playSdWav1.play("WEEK1.WAV");
               Serial.println("Button B Pressed");
               delay(500);
           }
 
-          if (CBut.IsPressed())
+          if (CBut->IsPressed())
           {
               playSdWav1.play("WEEK2.WAV");
               Serial.println("Button C Pressed");
               delay(500);
           }
 
-          if (DBut.IsPressed())
+          if (DBut->IsPressed())
           {
               playSdWav1.play("WEEK3.WAV");
               Serial.println("Button D Pressed");
               delay(500);
           }
 
-          if (EBut.IsPressed())
+          if (EBut->IsPressed())
           {
             playSdWav1.play("WEEK4.WAV");
             Serial.println("Button E Pressed");
             delay(500);
           }
 
-          if (FBut.IsPressed())
+          if (FBut->IsPressed())
           {
               playSdWav1.play("Gladiat.wav");
               Serial.println("Button F Pressed");
