c5f8469915a57fa960e5996bc98e65942f113b06
mj0ln1r42 <baileybp4@gmail.com>
2022-08-14 19:42:29 -0400

    Reorganize display.h

--- include/display/display.h
+++ include/display/display.h
@@ -12,6 +12,20 @@ holds the strips
 namespace kss {
 namespace display {
 
+void Blend(CRGB& pixel, const CRGB* blend_color, const float blend_factor) {
+  const auto unblend_factor = 1 - blend_factor;
+
+  // get weighted blend values values
+  const auto red = pixel.red * unblend_factor + blend_color->red * blend_factor;
+  const auto green =
+      pixel.green * unblend_factor + blend_color->green * blend_factor;
+  const auto blue =
+      pixel.blue * unblend_factor + blend_color->blue * blend_factor;
+
+  // set the new values onto the pixel
+  pixel.setRGB(red, green, blue);
+}
+
 class Display {
  public:
   const size_t strip_count;
@@ -20,35 +34,28 @@ class Display {
   Display(const size_t numberOfStrips, const size_t lengthOfStrips)
       : strip_count(numberOfStrips), strip_length(lengthOfStrips) {}
 
+  // Reference to the desired CRGB pixel for get/set and other operations
   virtual inline CRGB& Pixel(const size_t strip, const size_t pixel) = 0;
 
-  void BlendPixel(int x, int y, CRGB* blend_color, float blend_factor) {
-    if (y >= 0 && y < strip_length) {
-      CRGB& pixel = Pixel(x, y);
-      const auto unblend_factor = 1 - blend_factor;
+  // Draw this Display object's contents to the LED strips
+  virtual void Show() = 0;
 
-      // get weighted blend values values
-      const auto red =
-          pixel.red * unblend_factor + blend_color->red * blend_factor;
-      const auto green =
-          pixel.green * unblend_factor + blend_color->green * blend_factor;
-      const auto blue =
-          pixel.blue * unblend_factor + blend_color->blue * blend_factor;
-
-      // set the new values onto the pixel
-      pixel.setRGB(red, green, blue);
+  // Blend the pixel at (x, y) with blend_color according to a blend_factor
+  void BlendPixel(const int x, const int y, const CRGB* blend_color,
+                  const float blend_factor) {
+    if (CheckLocation(x, y)) {
+      Blend(Pixel(x, y), blend_color, blend_factor);
     }
   }
 
-  void DitherPixel(int stripIndex, float y, CRGB* color,
-                   float blend_factor = 1) {
+  // Dither the pixels on the strip above and below y, blending them to color
+  void DitherPixel(const int stripIndex, const float y, const CRGB* color,
+                   const float blend_factor = 1) {
     const int y_int = y;
     const float dither = y - y_int;
     BlendPixel(stripIndex, y_int, color, (1 - dither) * blend_factor);
     BlendPixel(stripIndex, y_int + 1, color, dither * blend_factor);
   }
-
-  virtual void Show() = 0;
 
  protected:
   inline bool CheckLocation(size_t strip, size_t pixel) {
