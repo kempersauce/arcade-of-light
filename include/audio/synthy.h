cb5c93ccbeaf849f476f44362f28da9f17478dec
Jon Bedell <JonBedell2@gmail.com>
2022-09-13 22:24:42 -0400

    added 6 voice version

--- include/audio/synthy.h
+++ include/audio/synthy.h
@@ -17,9 +17,15 @@ namespace audio {
 namespace _synthy {
 AudioSynthWaveform waveform1;   // xy=171,84 PAUL SHIT
 AudioSynthWaveform waveform2;   // xy=178,148 PAUL SHIT
+AudioSynthWaveform waveform3;   // xy=171,84 PAUL SHIT
+AudioSynthWaveform waveform4;   // xy=178,148 PAUL SHIT
+AudioSynthWaveform waveform5;   // xy=171,84 PAUL SHIT
+AudioSynthWaveform waveform6;   // xy=178,148 PAUL SHIT
 AudioOutputI2S i2s1;            // xy=360,98 PAUL SHIT
 AudioOutputAnalogStereo dacs1;  // xy=372,173
 AudioMixer4 effectMixer;
+AudioMixer4 mixer1;
+AudioMixer4 mixerMaster;
 
 // CHORUS EFFECT
 AudioEffectChorus   l_chorusEffect;
@@ -32,20 +38,28 @@ short r_delayline[CHORUS_DELAY_LENGTH];
 // number of "voices" in the chorus which INCLUDES the original voice
 int n_chorus = 2;
 
-//only send moving note to chorus effect 
+// only send moving note to chorus effect 
 AudioConnection patchCordChorusL(waveform1, 0, l_chorusEffect, 0);
 AudioConnection patchCordChorusR(waveform1, 1, r_chorusEffect, 1);
+// Effects Mixer
 AudioConnection patchCordChorusLOut(waveform1, 0, effectMixer, 0);
 AudioConnection patchCordChorusROut(waveform1, 1, effectMixer, 1);
-
-
 AudioConnection patchCordRawWave2L(waveform2, 0, effectMixer, 2);
 AudioConnection patchCordRawWave2R(waveform2, 1, effectMixer, 3);
+//mixer1 - input from other 4
+AudioConnection patchCordRawWave3(waveform3, 0, mixer1, 0);
+AudioConnection patchCordRawWave4(waveform4, 0, mixer1, 1);
+AudioConnection patchCordRawWave5(waveform5, 0, mixer1, 2);
+AudioConnection patchCordRawWave6(waveform6, 0, mixer1, 3);
+//mixerMaster - put it all togeter
+AudioConnection patchCordRawWave3(mixer1, 0, mixerMaster, 0);
+AudioConnection patchCordRawWave4(effectMixer, 1, mixerMaster, 1);
 
-AudioConnection patchCord1(effectMixer, 0, i2s1, 0);
-AudioConnection patchCord2(effectMixer, 0, i2s1, 1);
-AudioConnection patchCord3(effectMixer, 0, dacs1, 0);
-AudioConnection patchCord4(effectMixer, 0, dacs1, 1);
+// final output
+AudioConnection patchCord1(mixerMaster, 0, i2s1, 0);
+AudioConnection patchCord2(mixerMaster, 0, i2s1, 1);
+AudioConnection patchCord3(mixerMaster, 0, dacs1, 0);
+AudioConnection patchCord4(mixerMaster, 0, dacs1, 1);
 
 AudioControlSGTL5000 sgtl5000_1;  // xy=239,232
 }  // namespace synth
@@ -54,9 +68,19 @@ using namespace _synthy;
 class Synthy {
  public:
   float frequency = 256;  // middle C
+  
+  // NOTE REFERENCE: https://pages.mtu.edu/~suits/notefreqs.html
   float sequence[4] = {256, 311.13, 369.99, 554.37};
+  float cMaj7[6] = {256.00, 329.63, 392.00, 493.88, 659.25, 783.99};
   uint32_t next_hit = 0;
   unsigned long last_time = millis();
+
+  float wave1Amplitude = 0;
+  float wave2Amplitude = 0;
+  float wave3Amplitude = 0;
+  float wave4Amplitude = 0;
+  float wave5Amplitude = 0;
+  float wave6Amplitude = 0;
 
   size_t i = 0;
 
@@ -75,11 +99,20 @@ class Synthy {
     sgtl5000_1.enable();
     sgtl5000_1.volume(0.8);  // caution: very loud - use oscilloscope only!
 
-    // configure both waveforms for 440 Hz and maximum amplitude
-    waveform1.frequency(440);
-    waveform2.frequency(440);
-    waveform1.amplitude(1.0);
-    waveform2.amplitude(1.0);
+    // set up dat chord
+    waveform1.frequency(cMaj7[0]);
+    waveform2.frequency(cMaj7[1]);
+    waveform3.frequency(cMaj7[2]);
+    waveform4.frequency(cMaj7[3]);
+    waveform5.frequency(cMaj7[4]);
+    waveform6.frequency(cMaj7[5]);
+
+    waveform1.amplitude(wave1Amplitude);
+    waveform2.amplitude(wave2Amplitude);
+    waveform3.amplitude(wave3Amplitude);
+    waveform4.amplitude(wave4Amplitude);
+    waveform5.amplitude(wave5Amplitude);
+    waveform6.amplitude(wave6Amplitude);
 
     waveform1.begin(WAVEFORM_TRIANGLE);
 
@@ -127,16 +160,24 @@ class Synthy {
     next_hit = now + 500;
   
 
-  waveform1.frequency(sequence[i]);
-  waveform2.frequency(391.995);
-
-  // waveform1.amplitude(1.0);
-  // waveform2.amplitude(1.0);
+  // waveform1.frequency(sequence[i]);
+  // waveform2.frequency(391.995);
+  AudioNoInterrupts();
+  waveform1.amplitude(wave1Amplitude);
+  waveform2.amplitude(wave2Amplitude);
+  waveform3.amplitude(wave3Amplitude);
+  waveform4.amplitude(wave4Amplitude);
+  waveform5.amplitude(wave5Amplitude);
+  waveform6.amplitude(wave6Amplitude);
   AudioInterrupts();
 
   AudioNoInterrupts();
-  waveform1.begin(0);
-  waveform2.begin(WAVEFORM_SINE);
+  waveform1.begin(WAVEFORM_SAWTOOTH);
+  waveform2.begin(WAVEFORM_SAWTOOTH);
+  waveform3.begin(WAVEFORM_SAWTOOTH);
+  waveform4.begin(WAVEFORM_SAWTOOTH);
+  waveform5.begin(WAVEFORM_SAWTOOTH);
+  waveform6.begin(WAVEFORM_SAWTOOTH);
   AudioInterrupts();
   debug::println("played note");
   //add effect
