99a4ee77f0cb3af7392455453bbb79dd46721601
mj0ln1r42 <baileybp4@gmail.com>
2022-09-15 22:07:55 -0400

    Music Notes

---
+++ include/audio/music_notes.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace kss {
+namespace audio {
+
+// Reference: https://www.seventhstring.com/resources/notefrequencies.html
+float C[] {16.35, 32.70, 65.41, 130.8, 261.6, 523.3, 1047, 2093, 4186};
+float Cs[] {};
+float D[] {};
+float Eb[] {};
+float E[] {};
+float F[] {};
+float Fs[] {};
+float G[] {};
+float Gs[] {};
+float A[] {};
+float Bb[] {};
+float B[] {};
+
+}
+}
