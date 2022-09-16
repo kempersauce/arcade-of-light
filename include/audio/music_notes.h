7dbb690726c5da6592de5d8bd7934bffbd9cf8c8
mj0ln1r42 <baileybp4@gmail.com>
2022-09-15 22:20:40 -0400

    Fill out notes

--- include/audio/music_notes.h
+++ include/audio/music_notes.h
@@ -2,20 +2,24 @@
 
 namespace kss {
 namespace audio {
+namespace notes {
 
 // Reference: https://www.seventhstring.com/resources/notefrequencies.html
-float C[] {16.35, 32.70, 65.41, 130.8, 261.6, 523.3, 1047, 2093, 4186};
-float Cs[] {};
-float D[] {};
-float Eb[] {};
-float E[] {};
-float F[] {};
-float Fs[] {};
-float G[] {};
-float Gs[] {};
-float A[] {};
-float Bb[] {};
-float B[] {};
+// clang-format off
+constexpr float C[9]{16.35, 32.70, 65.41, 130.8, 261.6, 523.3, 1047, 2093, 4186};
+constexpr float Cs[9]{17.32, 34.65, 69.30, 138.6, 277.2, 554.4, 1109, 2217, 4435};
+constexpr float D[9]{18.35, 36.71, 73.42, 146.8, 293.7, 587.3, 1175, 2349, 4699};
+constexpr float Eb[9]{19.45, 38.89, 77.78, 155.6, 311.1, 622.3, 1245, 2489, 4978};
+constexpr float E[9]{20.60, 41.20, 82.41, 164.8, 329.6, 659.3, 1319, 2637, 5274};
+constexpr float F[9]{21.83, 43.65, 87.31, 174.6, 349.2, 698.5, 1397, 2794, 5588};
+constexpr float Fs[9]{23.12, 46.25, 92.50, 185.0, 370.0, 740.0, 1480, 2960, 5920};
+constexpr float G[9]{24.50, 49.00, 98.00, 196.0, 392.0, 784.0, 1568, 3136, 6272};
+constexpr float Gs[9]{25.96, 51.91, 103.8, 207.7, 415.3, 830.6, 1661, 3322, 6645};
+constexpr float A[9]{27.50, 55.00, 110.0, 220.0, 440.0, 880.0, 1760, 3520, 7040};
+constexpr float Bb[9]{29.14, 58.27, 116.5, 233.1, 466.2, 932.3, 1865, 3729, 7459};
+constexpr float B[9]{30.87, 61.74, 123.5, 246.9, 493.9, 987.8, 1976, 3951, 7902};
+// clang-format on
 
-}
-}
+}  // namespace notes
+}  // namespace audio
+}  // namespace kss
