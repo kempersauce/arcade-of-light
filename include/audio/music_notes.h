638ad9105fddc48ab96868174a49fb75d51a7d92
Jon Bedell <JonBedell2@gmail.com>
2022-09-15 22:29:36 -0400

    added some scales

--- include/audio/music_notes.h
+++ include/audio/music_notes.h
@@ -17,5 +17,12 @@ float A[] {};
 float Bb[] {};
 float B[] {};
 
+float C_Pentatonic[10] { C[3], D[3], F[3], G[3], A[3],
+                       C[4], D[4], F[4], G[4], A[4]};
+
+float Cs_HarmonicMinor[21] {Cs[3], Eb[3], E[3], Fs[3], Gs[3], A[3], C[3],
+                  Cs[4], Eb[4], E[4], Fs[4], Gs[4], A[4], C[4],
+                  Cs[5], Eb[5], E[5], Fs[5], Gs[5], A[5], C[5],};
+
 }
 }
