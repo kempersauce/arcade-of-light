fd1132c33b0e5e2dd80ca5e95ab85cae07388c70
mj0ln1r42 <baileybp4@gmail.com>
2022-09-18 23:47:21 -0400

    Update SenderSynth so send all controllers on all channels

--- include/mains/audio/SynthSender.cpp
+++ include/mains/audio/SynthSender.cpp
@@ -8,27 +8,28 @@
 #include "controls/dir_pad.h"          // for Inputs
 #include "controls/hardware/matrix.h"  // for controls::hardware::Matrix
 #include "serial/debug.h"              // for Debug
+#include "serial/hw_serials.h"         // for kHwSerials
 #include "serial/transmitter.h"        // for Transmitter
 
 using namespace kss;
 
 controls::hardware::Matrix control_context;
-audio::SynthSender* synth;
+audio::SynthSender* synths[4];
 
 void setup() {
   Debug_init();
   pins::Init();
 
-  synth = new kss::audio::SynthSender(
-    // clang-format off
-    control_context.CreateButton(pins::Controllers[0], pins::Buttons[4]),
-    control_context.CreateButton(pins::Controllers[0], pins::Buttons[3]),
-    control_context.CreateButton(pins::Controllers[0], pins::Buttons[5]),
-    control_context.CreateButton(pins::Controllers[0], pins::Buttons[2]),
-    control_context.CreateButton(pins::Controllers[0], pins::Buttons[1]),
-    control_context.CreateButton(pins::Controllers[0], pins::Buttons[0])
-    // clang-format on
-  );
+  for (size_t i = 0; i < 4; ++i) {
+    synths[i] = new audio::SynthSender(
+        control_context.CreateButton(pins::Controllers[i], pins::Buttons[0]),
+        control_context.CreateButton(pins::Controllers[i], pins::Buttons[1]),
+        control_context.CreateButton(pins::Controllers[i], pins::Buttons[2]),
+        control_context.CreateButton(pins::Controllers[i], pins::Buttons[3]),
+        control_context.CreateButton(pins::Controllers[i], pins::Buttons[4]),
+        control_context.CreateButton(pins::Controllers[i], pins::Buttons[5]),
+        serial::kHwSerials[i]);
+  }
 
   Debug("ready 2 goooo");
 }
@@ -37,7 +38,9 @@ void loop() {
   //   Debug("Loops brother");
   control_context.PollAll();
   //   Debug("Poll'd");
-  synth->checkButtonChange();
+  for (auto synth : synths) {
+    synth->checkButtonChange();
+  }
   //   Debug("Next Loop!");
 
   // Emulate ~150 fps
