ded706798bd3f2757685b1658c7773c484f8fbe9
mj0ln1r42 <baileybp4@gmail.com>
2022-09-22 15:48:54 -0400

    Fix electric arc timing

--- include/animation/electric_arc.h
+++ include/animation/electric_arc.h
@@ -6,11 +6,14 @@
 #include "animation/animation.h"  // for Animation
 #include "display/display.h"      // for Display
 #include "math/random.h"          // for random::*
+#include "time/now.h"             // for Now
 
 namespace kss {
 namespace animation {
 
 class ElectricArc : public Animation {
+  constexpr uint32_t update_speed{33};
+  uint32_t last_changed;
   std::vector<size_t> arc;
   CRGB color = CRGB::Purple;
 
@@ -19,22 +22,26 @@ class ElectricArc : public Animation {
   size_t magnitude = 2;
 
   void draw(display::Display* display) override {
-    do {
-      arc.clear();
-      arc.push_back(yLocation);
-      for (size_t y = yLocation, x = 1; x < display->size.x; x++) {
-        float r = math::random::Float();
-        if (r <= 0.4) {  // bottom 40%
-          y += magnitude;
-        } else if (r >= 0.6) {  // top 40%
-          y -= magnitude;
-        } else {  // middle 20%
-          // nothing
-        }
+    const uint32_t now = time::Now();
+    if (now - last_changed < update_speed) {
+      last_changed = now;
+      do {
+        arc.clear();
+        arc.push_back(yLocation);
+        for (size_t y = yLocation, x = 1; x < display->size.x; x++) {
+          float r = math::random::Float();
+          if (r <= 0.4) {  // bottom 40%
+            y += magnitude;
+          } else if (r >= 0.6) {  // top 40%
+            y -= magnitude;
+          } else {  // middle 20%
+            // nothing
+          }
 
-        arc.push_back(y);
-      }
-    } while (arc.back() != yLocation);
+          arc.push_back(y);
+        }
+      } while (arc.back() != yLocation);
+    }
 
     for (size_t i = 0; i < arc.size(); i++) {
       size_t arcHeight = arc[i];
