4f131c854e82c60e162cfe2a757a23a3ee390dc8
Jon Bedell <JonBedell2@gmail.com>
2022-09-29 22:51:31 -0400

    charge_bar draft

--- include/animation/charge_bar.h
+++ include/animation/charge_bar.h
@@ -7,24 +7,25 @@ namespace kss {
 namespace animation {
 
 // Single color background
-class SingleColorBlock : public Animation {
-  const size_t height_min;
-  const size_t height_max;
-
+class ChargeBar : public Animation {
+  const size_t height_min = 0;
   const CRGB color;
 
  public:
-  SingleColorBlock(int height_min, int height_max, CRGB color)
+  const size_t height;
+  const float blendFactor = 0.5;
+
+
+  ChargeBar(CRGB color)
       : Animation(),
-        height_min{height_min},
-        height_max{height_max},
         color{color} {}
 
   void draw(display::Display* display) {
-    for (size_t x = 0; x < display->size.x; ++x) {
-      for (size_t y = height_min; y < height_max; ++y) {
-        display->Pixel(x, y) = color;
-      }
+    //TODO: we could use dithering here 
+    totalHeight = unitsFilled * unitHeight;
+    for (size_t y = 0; y < height; ++y) {
+      display->BlendPixel(0, y, color, blendFactor);
+      display->BlendPixel(display->size.x - 1, y, color, blendFacor);
     }
   }
 };
