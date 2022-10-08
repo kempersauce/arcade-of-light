bc315d9731d2619a0d9a39f032a485ad4968424b
mj0ln1r42 <baileybp4@gmail.com>
2022-10-08 19:26:45 -0400

    Emit error message when failing to play a drum wave file

--- include/audio/drum_wav.h
+++ include/audio/drum_wav.h
@@ -2,8 +2,8 @@
 
 #include <Audio.h>
 
-#include "serial/debug.h"  // for Debug
 #include "math/random.h"   // for random::*
+#include "serial/debug.h"  // for Debug
 
 namespace kss {
 namespace audio {
@@ -26,7 +26,6 @@ AudioMixer4 mixerMain;
 
 AudioAmplifier amp;
 
-
 AudioOutputI2S i2s1;  // xy=360,98 PAUL SHIT
 
 AudioConnection patchCord1(drum1, 0, mixer1, 0);
@@ -36,7 +35,6 @@ AudioConnection patchCord4(drum4, 0, mixer1, 3);
 AudioConnection patchCord5(drum5, 0, mixer2, 0);
 AudioConnection patchCord6(drum6, 0, mixer2, 1);
 AudioConnection patchCordGlitch(glitchDrum, 0, mixer2, 2);
-
 
 AudioConnection patchCordMain1(mixer1, 0, mixerMain, 0);
 AudioConnection patchCordMain2(mixer2, 0, mixerMain, 1);
@@ -51,12 +49,18 @@ AudioConnection patchCordOut1(amp, 0, i2s1, 0);
 AudioConnection patchCordOut2(amp, 0, i2s1, 1);
 
 class DrumWav {
-
-  const String glitches[6] = {"GLITCH1.WAV", "GLITCH2.WAV", "GLITCH3.WAV", "GLITCH4.WAV", "GLITCH5.WAV", "GLITCH6.WAV"};
+  const char* glitches[6] = {"GLITCH1.WAV", "GLITCH2.WAV", "GLITCH3.WAV",
+                             "GLITCH4.WAV", "GLITCH5.WAV", "GLITCH6.WAV"};
   const uint8_t total_glitches = 6;
 
  public:
   DrumWav() { Debug("Pa rum pa pa pum"); }
+
+  inline const void PlayDrum(AudioPlaySdWav& drum, const char* filename) {
+    if (!drum.play(filename)) {
+      Debug("ERROR: unable to play file \"" + filename + "\"");
+    }
+  }
 
   const void drumSetup() {
     mixer1.gain(0, 0.2);
@@ -67,41 +71,39 @@ class DrumWav {
     mixer2.gain(1, 0.2);
 
     reverbMixer.gain(0, 0.8);
-    reverbMixer.gain(1, 0.2); // reverb wet
+    reverbMixer.gain(1, 0.2);  // reverb wet
     reverb.roomsize(0.7);
     reverb.damping(0.5);
 
     amp.gain(1);
 
-
-
-    drum1.play("808CLAP.WAV");
+    PlayDrum(drum1, "808CLAP.WAV");
     delay(500);
-    drum2.play("808HHOPN.WAV");
+    PlayDrum(drum2, "808HHOPN.WAV");
     delay(500);
-    drum3.play("808TOMH.WAV");
+    PlayDrum(drum3, "808TOMH.WAV");
     delay(500);
-    drum4.play("808HHCL.WAV"); 
+    PlayDrum(drum4, "808HHCL.WAV");
     delay(500);
-    drum5.play("808BD01.WAV");
+    PlayDrum(drum5, "808BD01.WAV");
     delay(500);
-    drum6.play("808SNR.WAV"); 
+    PlayDrum(drum6, "808SNR.WAV");
     delay(500);
   }
 
-  const void playDrum1() { drum1.play("808CLAP.WAV"); }
-  const void playDrum2() { drum2.play("808CRSH2.WAV"); }
+  const void playDrum1() { PlayDrum(drum1, "808CLAP.WAV"); }
+  const void playDrum2() { PlayDrum(drum2, "808CRSH2.WAV"); }
 
   const void stopDrum2() { drum2.stop(); }
-  const void playDrum3() { drum3.play("808TOMH.WAV"); }
-  const void playDrum4() { drum4.play("808HHCL.WAV"); }
-  const void playDrum5() { drum5.play("808BD01.WAV"); }
-  const void playDrum6() { drum6.play("808SNR.WAV"); }
+  const void playDrum3() { PlayDrum(drum3, "808TOMH.WAV"); }
+  const void playDrum4() { PlayDrum(drum4, "808HHCL.WAV"); }
+  const void playDrum5() { PlayDrum(drum5, "808BD01.WAV"); }
+  const void playDrum6() { PlayDrum(drum6, "808SNR.WAV"); }
   const void triggerGlitch() {
-    int glitch_num = math::random::Int8(0, total_glitches);
-    String glitch_name = glitches[glitch_num];
+    const size_t glitch_num = math::random::Int8(total_glitches);
+    const char* glitch_name = glitches[glitch_num];
     Debug("GLITCH:: " + glitch_num);
-    glitchDrum.play(glitch_name.c_str());
+    PlayDrum(glitchDrum, glitch_name);
   }
 
 };  // class DrumWav
