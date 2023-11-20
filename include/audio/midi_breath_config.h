c77180a6f153c4abfa472f239f780728db24626a
Jon Bedell <Jonbedell2@gmail.com>
2023-11-19 20:30:43 -0500

    I hate Strings in C++

--- include/audio/midi_breath_config.h
+++ include/audio/midi_breath_config.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <string.h>
+#include <stdio.h>
+#include "serial/debug.h"        // for Debug
 
 namespace kss {
 namespace audio {
@@ -9,25 +12,65 @@ namespace audio {
     // 2 Events per Chime - MIDI velocity > or < 75
 
     //CLUSTER 1
-    constexpr char kChimeStrike1[12] = "NATPAD.WAV";
-    constexpr char kChimeStrike2[12] = "NATPAD.WAV";
-    const char kChimeStrike3[12] = "NATPAD.WAV";
-    constexpr char kChimeStrike4[12] = "NATPAD.WAV";
-    constexpr char kChimeStrike5[12] = "NATPAD.WAV";
-    constexpr char kChimeStrike6[12] = "NATPAD.WAV";
+    constexpr char kChimeStrike1[13] = "808CLAP.WAV";
+    constexpr char kChimeStrike2[13] = "808CLAV.WAV";
+    constexpr char kChimeStrike3[13] = "808BD01.WAV";
+    constexpr char kChimeStrike4[13] = "808BD02.WAV";
+    constexpr char kChimeStrike5[13] = "808CRSH1.WAV";
+    constexpr char kChimeStrike6[13] = "808CRSH2.WAV";
 
-    constexpr char kChimeSway1[12] = "NATPAD.WAV";
-    constexpr char kChimeSway2[12] = "NATPAD.WAV";
-    constexpr char kChimeSway3[12] = "NATPAD.WAV";
-    constexpr char kChimeSway4[12] = "NATPAD.WAV";
-    constexpr char kChimeSway5[12] = "NATPAD.WAV";
-    constexpr char kChimeSway6[12] = "NATPAD.WAV";
+    constexpr char kChimeSway1[13] = "MOONJAZZ.WAV";
+    constexpr char kChimeSway2[13] = "JUPITER.WAV";
+    constexpr char kChimeSway3[13] = "EARTH.WAV";
+    constexpr char kChimeSway4[13] = "MARS.WAV";
+    constexpr char kChimeSway5[13] = "PLUTO.WAV";
+    constexpr char kChimeSway6[13] = "THRUST1.WAV";
 
-    const char clusterStrike1[6][12] {*kChimeStrike1, *kChimeStrike2, *kChimeStrike3, *kChimeStrike4, *kChimeStrike5, *kChimeStrike6 };
-    const char clusterSway1[6][12] {*kchimeSway1, *kchimeSway2, *kchimeSway3, *kchimeSway4, *kchimeSway5, *kchimeSway6};
+    const char clusterStrike1[13][6] {*kChimeStrike1, *kChimeStrike2, *kChimeStrike3, *kChimeStrike4, *kChimeStrike5, *kChimeStrike6 };
+    const char clusterSway1[13][6] {*kChimeSway1, *kChimeSway2, *kChimeSway3, *kChimeSway4, *kChimeSway5, *kChimeSway6};
 
+    static const char* getChimeAudioName(int channel, int chimeIndex, int velocity) {
+        bool isStrike = true;
+        const char* fileName;
+        if (velocity < 75 ) {
+            isStrike = false;
+        }
+        if(isStrike) {
+            const char* fileName = clusterStrike1[chimeIndex];         
+            Debug("FILENAME GOT: " + fileName);
+            return "808CLAP.WAV";
+        } else {
+            const char* fileName = clusterSway1[chimeIndex];
+            return "MOONJAZZ.WAV";
+        }
+        Debug("FILENAME GOT: " + fileName);
+        return fileName;
+    }
 
-    static void getChimeAudioName(int channel, int note, int velocity) {
+    static int noteSampleMapping(int note) {
+        switch(note) {
+            case 48:
+                return 0;
+
+            case 50:
+                return 1;
+            
+            case 52:
+                return 2;
+            
+            case 53:
+                return 3;
+            
+            case 55:
+                return 4;
+            
+            case 57:
+                return 5;
+
+            default:
+                Debug("Error: Unrecognized MIDI Note");
+                break;
+        }
 
     }
 
--- include/mains/audio/BreathOfLifeMidiReciever.cpp
+++ include/mains/audio/BreathOfLifeMidiReciever.cpp
@@ -9,6 +9,7 @@
 #include "serial/debug.h"        // for Debug
 #include "serial/ez_receiver.h"  // for EZReceiver
 #include "time/now.h"            // for time::*
+#include "audio/midi_breath_config.h"
 
 using namespace kss;
 using namespace kss::audio;
@@ -18,56 +19,26 @@ WavAudioManager wav_manager;
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial4, MIDI);
 
 
-void ProcessMessage(const WavAudioMessage& message) {
-  Channel& channel = wav_manager.GetChannel(message.channel_selector);
-  switch (message.action_selector) {
-    case kActionChannelPlay:
-      channel.Play(message.filename);
-      break;
-
-    case kActionChannelRepeat:
-      channel.Repeat(message.filename);
-      break;
-
-    case kActionChannelStop:
-      channel.Stop();
-      break;
-
-    case kActionChannelGain:
-      channel.SetGain(message.gain);
-      break;
-
-    default:
-      Debug("Error: Unrecognized channel action '" + message.action_selector +
-            "' (message: \"" + message.filename + "\")");
-      break;
-  }
-}
-
 void ProcessMidiMessage() {
-  int type, note, velocity, channel, d1, d2;
-  if (MIDI.read()) {
-    byte type = MIDI.getType();
-    switch (type) {
-      note = MIDI.getData1();
-      velocity = MIDI.getData2();
-      channel = MIDI.getChannel();
-      
-    }
+  int note, velocity, channel, wavChannel, d1, d2, chimeIndex;
+  byte type = MIDI.getType();
+  note = MIDI.getData1();
+  velocity = MIDI.getData2();
+  channel = MIDI.getChannel();
+  if(type == midi::NoteOn) {
+    Serial.println(String("Note On:  ch=") + channel + ", note=" + note + ", velocity=" + velocity);
 
+    chimeIndex = noteSampleMapping(note);
+    const char *wavFile = getChimeAudioName(channel, chimeIndex, velocity);
+
+    wavChannel = channel - 1;
+    char wavChannelChar = chimeIndex;
+    Channel& channel = wav_manager.PlayWav(wavFile);
+    // channel.Play(wavFile);
   }
+
 }
 
-serial::EZReceiver<WavAudioMessage> receiver{&Serial1};
-
-engines::FrameRate framerate;
-
-inline void StartChannel(uint8_t channel_index, char* filename, float gain) {
-  Channel& channel = wav_manager.GetChannelByIndex(channel_index);
-  channel.SetGainCoefficient(gain);
-  channel.SetGain(1);
-  channel.Repeat(filename);
-}
 
 //=============================================================================//
 // SETUP AND LOOP
@@ -79,10 +50,19 @@ void setup() {
   // Fire up the boombox
   audio::Init();
 
+
+  // Set gain on channels
+  float gain = 0.5;
+  float gainStandard = 0.5;
+  for(int channel_index = 0; channel_index < 6; channel_index++ ){
+    Channel& channel = wav_manager.GetChannel(channel_index);
+    channel.SetGain(gain);
+  }
+
   // Get Midi going
   MIDI.begin(MIDI_CHANNEL_OMNI);
   Serial.begin(57600);
-  Serial.println("MIDI Input Test");
+  Serial.println("MIDI Setup Complete");
 
   time::SetLoopTime();
   Channel& channel = wav_manager.PlayWav("FUEL50.WAV");
@@ -90,34 +70,15 @@ void setup() {
     time::SetLoopTime();
   }
 
-  uint8_t channel_index = 0;
-  if (!pins::ReadDip(0)) {
-    StartChannel(channel_index++, "BEE.WAV", 0.25f);
-    StartChannel(channel_index++, "FROGGY.WAV", 0.5f);
-    StartChannel(channel_index++, "OWL.WAV", 0.6f);
-    StartChannel(channel_index++, "NATPAD.WAV", 0.5f);
-    StartChannel(channel_index++, "STREAM.WAV", 0.5f);
-    StartChannel(channel_index++, "THUNDER.WAV", 0.45f);
-  } else {
-    StartChannel(channel_index++, "KILLDEER.WAV", 0.5f);
-    StartChannel(channel_index++, "KATYDID.WAV", 0.5f);
-    // StartChannel(channel_index++, "OWL.WAV", 0.6f);
-    // StartChannel(channel_index++, "NATPAD.WAV", 0.5f);
-    // StartChannel(channel_index++, "STREAM.WAV", 0.5f);
-    // StartChannel(channel_index++, "THUNDER.WAV", 0.5f);
-  }
-
   Debug("starting the loop");
 }
 
 void loop() {
   time::SetLoopTime();
+  if (MIDI.read()) {
+    ProcessMidiMessage();
+  }
   wav_manager.UpdateAll();
-  while (receiver.ReceiveMessages()) {
-  }
-  WavAudioMessage msg;
-  while (receiver.GetNextMessage(msg)) {
-    ProcessMessage(msg);
-  }
+
   //   framerate.PrintFrameRate();
 }
